559fdc1c5e0c956f04121c062843eaec71549a2c
Jack <44042705+Jackck1313@users.noreply.github.com>
2025-01-16 20:29:34 -0500

    Added sfx and music to song selection menu

--- Assets/Jack's Assets/JackScripts/SongSelector.cs
+++ Assets/Jack's Assets/JackScripts/SongSelector.cs
@@ -8,12 +8,15 @@ public class SongSelector : MonoBehaviour
     private int song_num;
     [SerializeField]
     private AudioSource song;
+    [SerializeField]
+    private AudioSource idle;
 
     // Start is called before the first frame update
     public void SelectSong()
     {
         SongManager.SongNum = song_num;
         song.Play();
+        idle.Stop();
     }
     public void DeselectSong()
     {
